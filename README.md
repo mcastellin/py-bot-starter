4a3e3abb723206f9f7b475564443f63946df26d3
Manuel Castellin <manuel@castellinconsulting.com>
2021-11-19 16:46:02 +0100

    feat: manage project dependencies with pipenv (#8)
    Now using pipenv to setup local development environment and cicd pipeline

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # py-bot-starter
 A Telegram bot framework to implement and deploy a bot in minutes
+
+# Installation
+
+Install the latest version of `py-bot-starter` with Pip:
+
+```bash
+pip install py-bot-starter
+```
--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
+    install_requires=[
+        "certifi==2021.10.8",
+        "charset-normalizer==2.0.7; python_version >= '3'",
+        "idna==3.3; python_version >= '3'",
+        "pymongo==3.12.1",
+        "pytelegrambotapi==4.2.0",
+        "requests==2.26.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4, 3.5'",
+        "urllib3==1.26.7; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4' and python_version < '4'",
+    ],
     name="py-bot-starter",
     version="0.0.0",
     author="Manuel Castellin",
-    author_email="castellin.manuel@gmail.com",
+    author_email="manuel@castellinconsulting.com",
     description="A Telegram bot framework to implement and deploy a bot in minutes",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,6 +30,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    packages=find_packages(where="src"),
+    python_requires=">=3.8",
 )
