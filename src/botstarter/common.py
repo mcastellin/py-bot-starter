5d3fb8b6c941ebfd6a83335b49c1368ad8297537
ceccatoandrea <73489171+ceccatoandrea@users.noreply.github.com>
2021-11-28 21:27:59 +0100

    feat: removed unused methods from common.py (#13)

--- src/botstarter/common.py
+++ src/botstarter/common.py
@@ -1,4 +1,6 @@
-import re
+"""
+A collection of useful functions
+"""
 
 
 def str2mdown(text):
@@ -9,94 +11,13 @@ def str2mdown(text):
         text (str): The string to be converted.
 
     Returns:
-        str: The Markdown converted text.
+        str or None: The Markdown converted text.
     """
-
-    return text.replace("-", "\\-") \
-        .replace("!", "\\!") \
-        .replace(".", "\\.") \
-        .replace(">", "\\>") \
-        .replace("_", "\\_")
-
-
-def get_str_token(instr, idx=0):
-    """
-     Returns the token at the specified index of the instr.
-
-     Parameters:
-         instr (str): The tokens string.
-         idx (int): The index of the token to be returned.
-
-     Returns:
-         str: The token at index idx of instr.
-     """
-
-    if instr and len(instr) > 0:
-        tokens = instr.lower().split()
-        if len(tokens) > idx:
-            return tokens[idx]
-        else:
-            return None
-    else:
+    if text is None:
         return None
-
-
-def extract_args(msg):
-    """
-     Returns the args list from a message.
-
-     Parameters:
-         msg (str): The message string.
-
-     Returns:
-         list[str]: The arguments list.
-     """
-
-    return msg.split()[1:]
-
-
-def extract_locale_string(locstring):
-    """
-     Extracts the locale string.
-
-     Parameters:
-         locstring (str): The string.
-
-     Returns:
-         str: The locale string.
-     """
-
-    if locstring is None:
-        return None
-    elif len(locstring) >= 2:
-        return locstring.lower()[:2]
     else:
-        return None
-
-
-def mask_text(text, pattern=None):
-    """
-    Substitutes {answer} in the pattern string with the masked text.
-
-     Parameters:
-         text (str): The text string to be masked.
-         pattern (str): The pattern string in which to insert the masked text.
-
-     Returns:
-         str: The pattern with masked text instead of {answer}.
-     """
-
-    masked = re.sub(r"\S", "*", text)
-    if pattern:
-        return pattern.format(answer=masked)
-    else:
-        return masked
-
-
-def unwrap_answer(text):
-    """
-    Replaces autocorrected characters from users' replies
-    :param text:
-    :return:
-    """
-    return text.replace("—", "--")
+        return text.replace("-", "\\-") \
+                   .replace("!", "\\!") \
+                   .replace(".", "\\.") \
+                   .replace(">", "\\>") \
+                   .replace("_", "\\_")
--- tests/botstarter/test_common.py
+++ tests/botstarter/test_common.py
@@ -1,13 +1,6 @@
 from botstarter.common import *
 
 
-def test_unwrap_answer():
-    user_input = "—opt"
-
-    result = unwrap_answer(user_input)
-    assert "--opt" == result, "User input was not unwrapped correctly."
-
-
 def test_str2mdown():
     str = "-!.>_"
 
@@ -15,41 +8,8 @@ def test_str2mdown():
     assert "\\-\\!\\.\\>\\_" == result, "Str was not converted to mdown correctly."
 
 
-def test_get_str_token():
-    instr = "a b c d"
+def test_empty_input_str2mdown():
+    str = None
 
-    result1 = get_str_token(instr, 2)
-    result2 = get_str_token(instr, 4)
-    result3 = get_str_token("")
-    assert 'c' == result1, "Token was not extracted correctly."
-    assert result2 is None, "Token index out of bounds wasn't handled correctly."
-    assert result3 is None, "Empty string wasn't handled correctly."
-
-
-def test_extract_args():
-    msg = "args b c"
-
-    result = extract_args(msg)
-    assert ['b', 'c'] == result, "Args were not extracted correctly."
-
-
-def test_extract_locale_string():
-    locstring = "it_IT"
-    short_locstring = "a"
-
-    result1 = extract_locale_string(locstring)
-    result2 = extract_locale_string(short_locstring)
-    result3 = extract_locale_string(None)
-    assert 'it' == result1, "Locale string was not extracted correctly."
-    assert result2 is None, "Short string wasn't handled correctly."
-    assert result3 is None, "None string wasn't handled correctly."
-
-
-def test_mask_text():
-    text = "a bc def"
-    pattern = "Test pattern: {answer}"
-
-    result1 = mask_text(text)
-    result2 = mask_text(text, pattern)
-    assert '* ** ***' == result1, "Text was not masked correctly."
-    assert 'Test pattern: * ** ***' == result2, "Pattern was not formatted correctly."
+    result = str2mdown(str)
+    assert result is None, "Empty input not handled correctly."
